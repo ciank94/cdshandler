ae7702b1b21a60c5610827a7a8139f1a1ef7b9da
Cian Kelly <cianrkelly23@gmail.com>
2024-12-23 14:21:40 +0100

    edited bulk data handler;

--- usage/bulk.py
+++ usage/bulk.py
@@ -3,7 +3,7 @@ years = ['2016']
 months = ['06']
 days = ['01'] #all
 output_path = './bulk_files'
-output_filename = f'bulk_file_{years[0]}_{months[0]}.nc'
+output_filename = f'bulk_file_{years[0]}-{months[0]}.nc'
 
 # ================request data===============
 request = {
